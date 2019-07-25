2418efbdc9bf62a7c6eb310113b8f638364c567d
Raul Lugo <lugo@sipgate.de>
2019-07-25 15:52:25 +0200

    🐛 returns correct array and remove console logs

--- src/com/rlugo14/Main.java
+++ src/com/rlugo14/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> allMasses = Arrays.asList(16, 1, 128);
-        int weight = 145;
+        List<Integer> allMasses = Arrays.asList(16, 1, 128, 20, 56, 100, 70, 3, 5, 10, 15, 45, 14, 4, 6);
+        int weight = 28;
         try {
             Scale.getMasses(weight, allMasses);
         } catch (NonWeightableException e) {
--- src/com/rlugo14/Scale.java
+++ src/com/rlugo14/Scale.java
@@ -1,11 +1,14 @@
 package com.rlugo14;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Scale {
 	static List<Integer> getMasses(Integer weight, List<Integer> allMasses) throws NonWeightableException{
 		List<Integer> massesRepetitions = new ArrayList<>(allMasses.size());
+		List<Integer> neededMasses = new ArrayList<>();
+		Collections.sort(allMasses);
 		List<Integer> desiredIndexes = new ArrayList<>();
 		
 		int sumOfAllMasses = allMasses.stream().mapToInt(mass -> mass).sum();
@@ -14,13 +17,19 @@ class Scale {
 			throw new NonWeightableException("Weight too big and therefore not weightable");
 		}
 		
+		if (allMasses.contains(weight)) {
+			Integer containedMass = allMasses.get(allMasses.indexOf(weight));
+			neededMasses.add(containedMass);
+			return neededMasses;
+		}
+		
 		while (weight > 0) {
 			for (Integer mass : allMasses) {
 				massesRepetitions.add(weight/mass);
 			}
 			
 			try {
-				int indexOfInterest = massesRepetitions.indexOf(1);
+				int indexOfInterest = massesRepetitions.lastIndexOf(1);
 				desiredIndexes.add(indexOfInterest);
 				int nonRepeatedMass = massesRepetitions.get(indexOfInterest);
 				int massToSubstract = allMasses.get(massesRepetitions.indexOf(nonRepeatedMass));
@@ -32,7 +41,10 @@ class Scale {
 			
 			massesRepetitions.clear();
 		}
-		System.out.println(desiredIndexes);
-		return massesRepetitions;
+		Collections.sort(desiredIndexes);
+		for(Integer index: desiredIndexes) {
+			neededMasses.add(allMasses.get(index));
+		}
+		return neededMasses;
 	}
 }
